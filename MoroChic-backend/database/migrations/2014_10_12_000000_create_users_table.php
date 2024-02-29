ee364c02a02beadd9b967af66ae42ab87cbe23db
elkhailihamza <elkhailihamza331@gmail.com>
2024-02-29 21:01:56 +0100

    added laravel sanctum to project for rest api, and added backend for both of login and registration forms

--- MoroChic-backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ MoroChic-backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username');
-            $table->string('firstname');
-            $table->string('lastname');
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
