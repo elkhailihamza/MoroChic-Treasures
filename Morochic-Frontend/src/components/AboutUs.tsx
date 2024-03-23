672a7ba0b902e7ad1949918a0de47ca2dd327f34
elkhailihamza <elkhailihamza331@gmail.com>
2024-03-23 14:45:14 +0000

    added some stuff to about us section

--- Morochic-Frontend/src/components/AboutUs.tsx
+++ Morochic-Frontend/src/components/AboutUs.tsx
@@ -19,15 +19,15 @@ export const AboutUs = () => {
           />
         </div>
         <div className="lg:w-2/5 w-4/5 mt-5">
-          <div className="flex flex-col justify-between lg:ms-6 h-full">
+          <div className="flex flex-col justify-between xl:ms-20 ms-6 w-full">
             <div className="md:text-[30px] text-[20px] font-medium">
               <p className="doppio-one-regular">Descover our legacy of</p>
               <p className="doppio-one-regular text-[#606C38] ms-7 -translate-y-3">
                 Craftsmanship and style
               </p>
             </div>
-            <div className="h-full w-full mb-4">
-              <p className="mt-3 md:mb-7">
+            <div className="h-full w-full xl:mb-4">
+              <p className="mt-3 xl:mb-7 mb-3">
                 Step into the rich tapestry of tradition with MoroChic, where
                 every garment is a testament to the beauty of cultural heritage.
               </p>
@@ -37,7 +37,7 @@ export const AboutUs = () => {
                 centuries-old techniques and artistic expression.
               </p>
             </div>
-            <div className="flex flex-col items-center lg:text-center gap-5 h-full mt-5 mb-10">
+            <div className="flex flex-col items-center lg:text-center gap-5 h-full xl:mt-5 md:mt-1 mt-5 mb-10">
               <p className="font-medium">
                 Click here to uncover the stories behind our brand and the
                 artisans who bring our vision to life.
