df26a06a974d493b4169a21f8e7713b5ae051c6a
elkhailihamza <elkhailihamza331@gmail.com>
2024-04-17 11:47:36 +0100

    added logout api route

--- MoroChic-backend/routes/api.php
+++ MoroChic-backend/routes/api.php
@@ -20,9 +20,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', 'logout');
+        Route::post('/me', 'me');
+    });
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/products/store', [ProductController::class, 'store']);
