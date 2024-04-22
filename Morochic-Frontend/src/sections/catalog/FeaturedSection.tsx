0765234ef26048c5d6149d29ec8637ce2bfd84b1
elkhailihamza <elkhailihamza331@gmail.com>
2024-04-22 11:30:12 +0100

    added some stuff to catalog page

--- Morochic-Frontend/src/sections/catalog/FeaturedSection.tsx
+++ Morochic-Frontend/src/sections/catalog/FeaturedSection.tsx
@@ -1,5 +1,4 @@
-import { FullSizeItemCard } from "../../components/FullSizeItemCard";
-import { MiniItemCard } from "../../components/MiniItemCard";
+import { ItemCard } from "../../components/ItemCard";
 
 export const FeaturedSection = () => {
   return (
@@ -7,12 +6,32 @@ export const FeaturedSection = () => {
       <div className="mt-16 lg:px-20">
         <h1 className="text-2xl">Featured</h1>
         <div className="flex flex-wrap gap-2 mt-3">
-          <FullSizeItemCard></FullSizeItemCard>
-          <div className="carousel flex gap-4">
-            <MiniItemCard className="carousel-item"></MiniItemCard>
-            <MiniItemCard className="carousel-item"></MiniItemCard>
-            <MiniItemCard className="carousel-item"></MiniItemCard>
-            <MiniItemCard className="carousel-item"></MiniItemCard>
+          <ItemCard base={false} className="w-full h-96 bg-gray-300"></ItemCard>
+          <div className="carousel flex w-full gap-4">
+            <div className="w-32 h-20">
+              <ItemCard
+                base={false}
+                className="bg-gray-300 carousel-item"
+              ></ItemCard>
+            </div>
+            <div className="w-32 h-20">
+              <ItemCard
+                base={false}
+                className="bg-gray-300 carousel-item"
+              ></ItemCard>
+            </div>
+            <div className="w-32 h-20">
+              <ItemCard
+                base={false}
+                className="bg-gray-300 carousel-item"
+              ></ItemCard>
+            </div>
+            <div className="w-32 h-20">
+              <ItemCard
+                base={false}
+                className="bg-gray-300 carousel-item"
+              ></ItemCard>
+            </div>
           </div>
         </div>
       </div>
