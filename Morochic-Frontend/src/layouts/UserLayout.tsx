186074ee7848105a92340805d2b91734af646809
elkhailihamza <elkhailihamza331@gmail.com>
2024-04-27 11:02:33 +0100

    some changes to userlayout to limit guest access to certain features

--- Morochic-Frontend/src/layouts/UserLayout.tsx
+++ Morochic-Frontend/src/layouts/UserLayout.tsx
@@ -4,13 +4,15 @@ import { Footer } from "../sections/Main/FooterSection";
 import { useUser } from "../contexts/UserContext";
 import { SpinnerCircular } from "spinners-react";
 import { useAuth } from "../contexts/AuthContext";
+import { useEffect } from "react";
+import { UNAUTHORIZED, router } from "../App";
 
 export const UserLayout = () => {
   const { isLoading } = useUser();
   const { isLoggedIn } = useAuth();
-  // useEffect(() => {
-  //   isLoggedIn && isLoading === false ? "" : router.navigate(UNAUTHORIZED);
-  // }, []);
+  useEffect(() => {
+    isLoggedIn && isLoading === false ? "" : router.navigate(UNAUTHORIZED);
+  }, []);
   return isLoading ? (
     <div className="h-screen flex justify-center items-center">
       <SpinnerCircular color="#000000" />
