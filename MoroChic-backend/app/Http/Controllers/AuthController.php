624b15648797f721d658dc74fee71fe0b4b933d9
elkhailihamza <elkhailihamza331@gmail.com>
2024-04-29 14:11:08 +0100

    added repo to project

--- MoroChic-backend/app/Http/Controllers/AuthController.php
+++ MoroChic-backend/app/Http/Controllers/AuthController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Profile;
-use App\Models\User;
+use App\Repositories\AuthRepositoryInterface;
 use Exception;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+    protected $authRepository;
+
+    public function __construct(AuthRepositoryInterface $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
     public function login(Request $request)
     {
         try {
@@ -23,24 +24,9 @@ class AuthController extends Controller
                 'password' => 'required',
             ]);
 
-            if (!Auth::attempt($data)) {
-                return response()->json([
-                    'errors' => [
-                        'message' => 'Wrong Credentials',
-                        'details' => 'Invalid username or password',
-                    ]
-                ], 400);
-            }
+            $response = $this->authRepository->login($data, $request);
 
-            $user = User::where('email', $request['email'])->firstOrFail();
-            $token = $user->createToken('auth_token')->plainTextToken;
-
-            return response()->json([
-                'message' => 'Successfully logged in!',
-                'access_token' => $token,
-                'user' => $user,
-                'token_type' => 'Bearer',
-            ], 200);
+            return response()->json($response, 200);
         } catch (ValidationException $e) {
             return response()->json([
                 'message' => 'Login Failed!',
@@ -48,6 +34,7 @@ class AuthController extends Controller
             ], 422);
         }
     }
+
     public function register(Request $request)
     {
         try {
@@ -57,58 +44,37 @@ class AuthController extends Controller
                 'password' => 'required|string|min:8|confirmed',
             ]);
 
-            DB::beginTransaction();
-            $user = User::create([
-                'username' => $request->input('username'),
-                'email' => $request->input('email'),
-                'password' => Hash::make($request->input('password')),
-                'role_id' => 1,
-            ]);
+            $response = $this->authRepository->register($request);
 
-            Profile::create([
-                'user_id' => $user->id,
-            ]);
-
-            if (!$user) {
-                throw new Exception('Failed to create user!');
-            }
-            DB::commit();
-
-            return response()->json([
-                'message' => 'Successfully created account!',
-            ], 201);
+            return response()->json($response, 201);
         } catch (ValidationException $e) {
             return response()->json([
                 'message' => 'Registration Failed!',
                 'errors' => $e->validator->errors(),
             ], 422);
         } catch (Exception $e) {
-            DB::rollBack();
             return response()->json([
                 'message' => 'Registration Failed!',
                 'errors' => $e->getMessage(),
             ], 500);
         }
     }
+
     public function logout(Request $request)
     {
         try {
-            $request->user()->tokens()->delete();
-            return response()->json(['message' => 'logged out successfully!'], 200);
+            $response = $this->authRepository->logout($request);
+
+            return response()->json($response, 200);
         } catch (Exception $e) {
             return response()->json(['error' => $e->getMessage()], 401);
         }
     }
-    public function me(Request $request)
-{
-    $currentUser = User::select('users.*', 'roles.role_name', 'profiles.avatar')
-        ->join('roles', 'users.role_id', '=', 'roles.id')
-        ->join('profiles', 'users.id', '=', 'profiles.user_id')
-        ->where('users.id', '=', $request->user()->id)
-        ->first();
 
-    return response()->json([
-        'user' => $currentUser,
-    ], 200);
-}
+    public function me(Request $request)
+    {
+        $response = $this->authRepository->me($request);
+
+        return response()->json($response, 200);
+    }
 }
